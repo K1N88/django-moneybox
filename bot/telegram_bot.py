ad62dd2277ef92b92e0f35cf89e2317ea905816a
K1N88 <m-end-eleev@yandex.ru>
2023-12-20 00:27:45 +0300

    add bot base3

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from random import randint
 
-from telebot.async_telebot import AsyncTeleBot, types
+from telebot import TeleBot, types
 from telebot.util import quick_markup
 
 from config import bot_token
@@ -15,201 +15,100 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-bot = AsyncTeleBot(bot_token)
+def get_wallets(user):
+    wallets = [
+        {
+            'name': 'RUB',
+            'currency': 'RUB',
+            'balance': 100.1,
+            'id': 1
+        },
+        {
+            'name': 'USD',
+            'currency': 'USD',
+            'balance': 200.1,
+            'id': 2
+        },
+        {
+            'name': 'EUR',
+            'currency': 'EUR',
+            'balance': 300.1,
+            'id': 3
+        },
+        {
+            'name': 'UAH',
+            'currency': 'UAH',
+            'balance': 400.1,
+            'id': 4
+        },
+    ]
+    return wallets
+
+
+bot = TeleBot(bot_token)
 logger.info('Бот успешно запущен.')
 
 
-@bot.message_handler(commands=['start'])
-async def welcome(message):
-    # Создание клавиатуры
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("wallets")
-
-    markup.add(item1)
-
-    # Отправка приветственного сообщения и клавиатуры
-    await bot.send_message(
-        message.chat.id,
-        "Привет, {0.first_name}! Я - <b>{1.first_name}</b>, бот созданный для того чтобы помочь тебе.".format(  # noqa
-            message.from_user, await bot.get_me()
-        ), parse_mode='html', reply_markup=markup
-    )
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    markup = quick_markup({
+        'Кошельки': {'callback_data': 'wallets'},
+        'Переводы': {'callback_data': 'transfers'},
+    }, row_width=2)
+    text = 'Привет! Выбери действие:'
+    bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
-# Обработка команды "Рандомное число"
-@bot.message_handler(content_types=['text'])
-async def random_number(message):
-    if message.chat.type == 'private':
-        if message.text == '🎲 Рандомное число':
-            # Генерация случайного числа от 0 до 100
-            await bot.send_message(message.chat.id, str(randint(0, 100)))
+@bot.callback_query_handler(func=lambda call: call.data == 'wallets')
+def wallets(call):
+    wllts = get_wallets(call.message.chat.id)
+    markup = 
+    for item in wllts:
 
-        elif message.text == '😊 Как дела?':
-            # Создание инлайн-клавиатуры
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
-            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
-
-            markup.add(item1, item2)
-
-            await bot.send_message(message.chat.id, 'Отлично, как у тебя?',
-                                   reply_markup=markup)
-
-        elif message.text == '🎁 Получить подарок':
-            # Создание клавиатуры с возможностью отправки локации
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.KeyboardButton("Отправить локацию",
-                                         request_location=True)
-
-            markup.add(item1)
-
-            await bot.send_message(
-                message.chat.id,
-                "Отправь мне свою локацию и я найду для тебя ближайший подарок!",  # noqa
-                reply_markup=markup
-            )
+    text = 'Выбери кошелек:'
+    bot.send_message(call.message.chat.id, text, reply_markup=markup)
 
 
-# Обработка нажатий на кнопки инлайн-клавиатуры
-@bot.callback_query_handler(func=lambda call: True)
-async def callback_inline(call):
+def process_name_step(message):
     try:
-        if call.message:
-            if call.data == 'good':
-                await bot.send_message(call.message.chat.id,
-                                       'Я рад, что у тебя все хорошо! 😊')
-            elif call.data == 'bad':
-                await bot.send_message(call.message.chat.id,
-                                       'Не переживай, все наладится! 😊')
 
-            # Удаление сообщения с инлайн-клавиатурой
-            await bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-                text="Отлично, как у тебя?",
-                reply_markup=None
-            )
-
+        msg = bot.reply_to(message, 'How old are you?')
+        bot.register_next_step_handler(msg, process_age_step)
     except Exception as e:
-        print(repr(e))
+        bot.reply_to(message, 'oooops')
 
 
-# Обработка отправки локации
-@bot.message_handler(content_types=['location'])
-async def handle_location(message):
-    # Создание клавиатуры с возможностью отправки контакта
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Отправить контакт", request_contact=True)
-
-    markup.add(item1)
-
-    await bot.send_message(
-        message.chat.id,
-        "Спасибо! Теперь отправь мне свой контакт, чтобы я мог связаться с тобой.",  # noqa
-        reply_markup=markup
-    )
-
-
-# Обработка отправки контакта
-@bot.message_handler(content_types=['contact'])
-async def handle_contact(message):
-    await bot.send_message(
-        message.chat.id,
-        "Спасибо за контакт! Я свяжусь с тобой в ближайшее время."
-    )
-
-
-# Запуск бота
-async def main():
-    await bot.polling(none_stop=True)
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
-
-
-import telebot
-from telebot import types
-
-# Создание бота
-bot = telebot.TeleBot('TOKEN')
-
-
-# Обработка команды /start
-@bot.message_handler(commands=['start'])
-def welcome(message):
-    # Создание клавиатуры
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("🎲 Рандомное число")
-    item2 = types.KeyboardButton("😊 Как дела?")
-    item3 = types.KeyboardButton("🎁 Получить подарок")
-
-    markup.add(item1, item2, item3)
-
-    # Отправка приветственного сообщения и клавиатуры
-    bot.send_message(
-        message.chat.id,
-        "Привет, {0.first_name}! Я - <b>{1.first_name}</b>, бот созданный для того чтобы помочь тебе.".format(  # noqa
-            message.from_user, bot.get_me()
-        ), parse_mode='html', reply_markup=markup
-    )
-
-
-# Обработка команды "Рандомное число"
-@bot.message_handler(content_types=['text'])
-def random_number(message):
-    if message.chat.type == 'private':
-        if message.text == '🎲 Рандомное число':
-            # Генерация случайного числа от 0 до 100
-            bot.send_message(message.chat.id, str(randint(0, 100)))
-
-        elif message.text == '😊 Как дела?':
-            # Создание инлайн-клавиатуры
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
-            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
-
-            markup.add(item1, item2)
-
-            bot.send_message(message.chat.id, 'Отлично, как у тебя?',
-                             reply_markup=markup)
-
-        elif message.text == '🎁 Получить подарок':
-            # Создание клавиатуры с возможностью отправки локации
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1 = types.KeyboardButton("Отправить локацию",
-                                         request_location=True)
-
-            markup.add(item1)
-
-            bot.send_message(
-                message.chat.id,
-                "Отправь мне свою локацию и я найду для тебя ближайший подарок!",  # noqa
-                reply_markup=markup
-            )
-
-
-# Обработка нажатий на кнопки инлайн-клавиатуры
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
+def process_age_step(message):
     try:
-        if call.message:
-            if call.data == 'good':
-                bot.send_message(call.message.chat.id,
-                                 'Я рад, что у тебя все хорошо! 😊')
-            elif call.data == 'bad':
-                bot.send_message(call.message.chat.id,
-                                 'Не переживай, все наладится! 😊')
-
-            # Удаление сообщения с инлайн-клавиатурой
-            bot.edit_message_text(chat_id=call.message.chat.id,
-                                  message_id=call.message.message_id,
-                                  text="Отлично, как у тебя?",
-                                  reply_markup=None)
-
+        chat_id = message.chat.id
+        age = message.text
+        if not age.isdigit():
+            msg = bot.reply_to(message, 'Age should be a number. How old are you?')
+            bot.register_next_step_handler(msg, process_age_step)
+            return
+        user = user_dict[chat_id]
+        user.age = age
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Male', 'Female')
+        msg = bot.reply_to(message, 'What is your gender', reply_markup=markup)
+        bot.register_next_step_handler(msg, process_sex_step)
     except Exception as e:
-        print(repr(e))
+        bot.reply_to(message, 'oooops')
 
 
-# Запуск бота
-bot.polling(none_stop=True)
+def process_sex_step(message):
+    try:
+        chat_id = message.chat.id
+        sex = message.text
+        user = user_dict[chat_id]
+        if (sex == u'Male') or (sex == u'Female'):
+            user.sex = sex
+        else:
+            raise Exception()
+        bot.send_message(message.chat.id, 'Nice to meet you ' + user.name + '\n Age:' + str(user.age) + '\n Sex:' + user.sex)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+
+bot.infinity_polling()
